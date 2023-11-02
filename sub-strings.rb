2e92bb01227b408a9aff8d0c0b7854886e7292fd
WILbeerT-B <wilbertbundalian@gmail.com>
2023-11-02 06:08:05 -0700

    Modify FOR loop to remove empty strings, use REDUCE method to sum up the matched  string count

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,19 +1,40 @@
 dictionary = 
 ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-word = "below"
-#if dictionary.include?(word)
- # puts word
-#end
+def substring(word, dictionary)
 
-word_array = []
-  
-for i in 0..word.length-1
-  for j in 0..word.length-1
-    if dictionary.include?(word[i..j].downcase)
-      word_array.push(word[i..j].downcase)
+  #word = "below"
+  #word = "Howdy partner, sit down! How's it going?"
+
+  word_size = word.length-1
+
+  #if dictionary.include?(word)
+  # puts word
+  #end
+
+  word_array = []
+    
+  for i in 0..word_size
+    for j in 0..word_size
+      
+      if i > 0
+        j += i
+      end
+
+      if dictionary.include?(word[i..j].downcase)
+        word_array.push(word[i..j].downcase)
+      end
+      #puts word[i..j]
     end
+    word_size -= 1
   end
+
+  output = ''
+  word_array.reduce(Hash.new(0)) do |result, item|
+    result[item] += 1
+    output = result
+  end
+  puts output
 end
 
-puts word_array
+substring("Howdy partner, sit down! How's it going?", dictionary)
