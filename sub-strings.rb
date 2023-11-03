36167072b570ba019e35dd0fb0e7d8c7ae150ecb
WILbeerT-B <wilbertbundalian@gmail.com>
2023-11-02 21:29:59 -0700

    Reformat code, add comments

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,35 +1,38 @@
+#array of substrings
 dictionary = 
 ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substring(word, dictionary)
-
-  #word = "below"
-  #word = "Howdy partner, sit down! How's it going?"
-
+def substrings(word, dictionary)
+  #length of the string in word
   word_size = word.length-1
-
-  #if dictionary.include?(word)
-  # puts word
-  #end
-
+  
+  #array to push all the matched strings
   word_array = []
-    
+
+  #iterate through the word  
   for i in 0..word_size
     for j in 0..word_size
       
+      #remove empty strings
       if i > 0
         j += i
       end
 
+      #check if part of the word is included in dictionary
       if dictionary.include?(word[i..j].downcase)
+        #push to array if matched
         word_array.push(word[i..j].downcase)
       end
-      #puts word[i..j]
+
     end
+    #decrement word size to remove empty strings
     word_size -= 1
   end
 
+  #variable that will hold the result
   output = ''
+
+  #reduce method to count each string in the word_array
   word_array.reduce(Hash.new(0)) do |result, item|
     result[item] += 1
     output = result
@@ -37,4 +40,4 @@ def substring(word, dictionary)
   puts output
 end
 
-substring("Howdy partner, sit down! How's it going?", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
